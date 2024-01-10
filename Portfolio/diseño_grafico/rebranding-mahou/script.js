8636c1cd77ac984d12be5eac4d2767ff3d1bb30b
ronaldherrera <ronaldcalzadilla31@gmail.com>
2024-01-10 15:15:28 +0100

    Actualización- añadiendo proyecto al portfolio

---
+++ Portfolio/diseño_grafico/rebranding-mahou/script.js
@@ -0,0 +1,9 @@
+function mostrarPopUpInfo() {
+  var popUp = document.getElementById("popUpInfo");
+  popUp.style.display = "flex";
+}
+
+function cerrarPopUpInfo() {
+  var popUp = document.getElementById("popUpInfo");
+  popUp.style.display = "none";
+}
