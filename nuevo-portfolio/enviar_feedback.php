a50f4ab29a4401d06ee061018ac431970cadf77f
ronaldherrera <ronaldcalzadilla31@gmail.com>
2024-03-18 22:36:08 +0100

    321

--- nuevo-portfolio/enviar_feedback.php
+++ nuevo-portfolio/enviar_feedback.php
@@ -1,27 +1,27 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recibir el feedback desde el formulario
-    $feedback = $_POST['feedback'];
-
-    // Dirección de correo electrónico a la que se enviará el feedback
-    $to = "ronaldherrera3d@gmail.com";
-
-    // Asunto del correo electrónico
-    $subject = "Feedback de la página web";
-
-    // Contenido del correo electrónico
-    $message = "Feedback recibido:\n\n" . $feedback;
-
-    // Cabeceras del correo electrónico
-    $headers = "From: hola@ronaldherrera.es" . "\r\n" .
-        "Reply-To: hola@ronaldherrera.es" . "\r\n" .
-        "X-Mailer: PHP/" . phpversion();
-
-    // Enviar el correo electrónico
-    if (mail($to, $subject, $message, $headers)) {
-        http_response_code(200); // OK
+    $correo_destino = "ronaldherrera3d@gmail.com";
+    $asunto = "Feedback de sitio web";
+    $mensaje = $_POST['imput-home'];
+    
+    // Encabezados
+    $headers = "From: feedback@tusitio.com" . "\r\n" .
+               "Reply-To: feedback@tusitio.com" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+    
+    // Enviamos el correo
+    if (mail($correo_destino, $asunto, $mensaje, $headers)) {
+        echo '<div class="success-message w-form-done">
+                <div class="text-block-10">
+                  ¡Gracias! Tu comentario me ayudará a mejorar, lo tendré en cuenta para mi próxima versión del sitio.
+                </div>
+              </div>';
     } else {
-        http_response_code(500); // Error interno del servidor
+        echo '<div class="error-message w-form-fail">
+                <div class="text-block-9">
+                  ¡Oops! Algo ha ido mal, vuelve a intentarlo más tarde.
+                </div>
+              </div>';
     }
 }
 ?>
