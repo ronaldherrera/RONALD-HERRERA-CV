531c6f56e2e6a0975a8dcbc4ec0067b5d59bfe18
ronaldherrera <ronaldcalzadilla31@gmail.com>
2024-03-28 17:16:57 +0100

    zasx

--- probando/Envio-feedback.php
+++ probando/Envio-feedback.php
@@ -1,28 +1,2 @@
-<?php
-// Recoger los datos del formulario
-$mensaje = $_POST['imput-home']; // El nombre del textarea del formulario
 
-// Establecer la dirección de correo electrónico de destino
-$destinatario = 'ronaldherrera3d@gmail.com';
-
-// Establecer el asunto del correo electrónico
-$asunto = 'Feedback desde el formulario de tu sitio web';
-
-// Establecer la dirección de correo electrónico del remitente (debe ser una dirección válida en tu servidor)
-$remitente = 'hola@ronaldherrera.es';
-
-// Cabeceras del correo electrónico
-$headers = "From: $remitente\r\n";
-$headers .= "Reply-To: $remitente\r\n";
-$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-
-// Enviar el correo electrónico
-if(mail($destinatario, $asunto, $mensaje, $headers)) {
-    // Envío exitoso
-    echo json_encode(array('status' => 'success'));
-} else {
-    // Error en el envío
-    echo json_encode(array('status' => 'error'));
-}
-?>
 
