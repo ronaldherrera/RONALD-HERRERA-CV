a15a5b315e02dc195c9e26fc0b1d5e8abce6abac
ronaldherrera <ronaldcalzadilla31@gmail.com>
2024-03-28 15:47:34 +0100

    d

--- probando/Envio-feedback.php
+++ probando/Envio-feedback.php
@@ -1,33 +1,24 @@
 <?php
+// Verificar si el método de solicitud es POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recoge los datos del formulario
-    $feedback = $_POST['imput-home']; // Contenido del feedback
-
-    // Dirección de correo electrónico a la que se enviará el feedback
-    $to = "hola@ronaldherrera.es";
-
+    // Dirección de correo a la que se enviará el feedback
+    $to = "ronaldherrera3d@gmail.com";
     // Asunto del correo
-    $subject = "FORMULARIO DE LA HOME";
+    $subject = "Feedback de tu sitio web";
+    // Mensaje del correo, obtenido del textarea del formulario
+    $message = $_POST['imput-home'];
+    // Encabezados del correo
+    $headers = "From: hola@ronalherrera.es";
 
-    // Cuerpo del correo
-    $message = "Feedback recibido:\n\n";
-    $message .= $feedback;
-
-    // Cabeceras del correo
-    $headers = "From: webmaster@example.com" . "\r\n" .
-               "Reply-To: webmaster@example.com" . "\r\n" .
-               "X-Mailer: PHP/" . phpversion();
-
-    // Envía el correo
+    // Intentar enviar el correo
     if (mail($to, $subject, $message, $headers)) {
-        // Si el correo se envía con éxito, muestra el mensaje de éxito
-        echo json_encode(array('success' => true));
+        // Si se envía correctamente, devolver un JSON con éxito verdadero
+        echo json_encode(array("success" => true));
     } else {
-        // Si hay un error en el envío del correo, muestra el mensaje de error
-        echo json_encode(array('success' => false));
+        // Si hay un error al enviar, devolver un JSON con éxito falso
+        echo json_encode(array("success" => false));
     }
 } else {
-    // Si no se accede mediante POST, muestra un error
-    echo json_encode(array('success' => false));
+    // Si se accede directamente al archivo PHP, redirigir al inicio
 }
 ?>
