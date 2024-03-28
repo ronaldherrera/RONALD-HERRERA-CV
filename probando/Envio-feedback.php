7038c7721721466d7e46f386b6d253bd6bc3865f
ronaldherrera <ronaldcalzadilla31@gmail.com>
2024-03-28 16:38:08 +0100

    658

--- probando/Envio-feedback.php
+++ probando/Envio-feedback.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = "ronaldherrera3d@gmail.com";
     $subject = "Feedback de tu sitio web: " . $_POST['data-name'];
     $message = $_POST['imput-home'];
-    $headers = "From: hola@ronalherrera.es";
+    $headers = "From: hola@ronaldherrera.es";
 
     if (mail($to, $subject, $message, $headers)) {
         echo json_encode(array("success" => true));
