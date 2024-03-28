e11b96c26f9f195ddd440dde14ea670810c570be
ronaldherrera <ronaldcalzadilla31@gmail.com>
2024-03-28 16:50:56 +0100

    s

--- probando/script.js
+++ probando/script.js
@@ -50,3 +50,111 @@ enlaces.forEach(function (enlace) {
 });
 
 /////////
+document.addEventListener("DOMContentLoaded", function () {
+  const isMobile = window.matchMedia("(max-width: 890px)").matches;
+  const lines = document.querySelectorAll(".animated-line");
+
+  if (!isMobile) {
+    document.addEventListener("mousemove", function (e) {
+      const mouseX = e.clientX / window.innerWidth;
+      const mouseY = e.clientY / window.innerHeight;
+
+      lines.forEach((line) => {
+        const moveAmount = line.getAttribute("data-move-amount");
+        const moveX = (mouseX - 0.5) * moveAmount;
+        const moveY = (mouseY - 0.5) * moveAmount;
+        line.style.transform = `translate(${moveX}px, ${moveY}px)`;
+      });
+    });
+  } else {
+    document.addEventListener("scroll", function () {
+      const scrollY = window.scrollY;
+
+      lines.forEach((line) => {
+        const scrollAmount = line.getAttribute("data-scroll-amount");
+        const translateY = scrollY * scrollAmount;
+        line.style.transform = `translateY(${translateY}px)`;
+      });
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const graficoFondo = document.querySelector(".grafico-fondo-proyecto");
+
+  document.addEventListener("mousemove", function (e) {
+    const mouseX = e.clientX;
+    const windowWidth = window.innerWidth;
+    const percentX = (mouseX / windowWidth) * 100;
+    const offset = (percentX - 50) * 10; // Ajusta la velocidad del movimiento cambiando el valor multiplicativo
+
+    graficoFondo.style.transform = `translateX(${offset}px)`;
+  });
+});
+
+//////////
+window.addEventListener("scroll", function () {
+  var scrollY = window.scrollY;
+
+  document.querySelectorAll(".aro").forEach(function (aro) {
+    var speed = parseInt(aro.classList[1].split("_")[1]);
+    var offset = scrollY * speed * 0.04;
+    aro.style.top = 234 + offset + "px"; // Cambio de dirección: + en lugar de -
+  });
+});
+
+////////////////
+// Esperar a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM completamente cargado");
+
+  // Seleccionar el formulario por su ID
+  var form = document.getElementById("wf-form-feedback");
+  console.log("Formulario seleccionado:", form);
+
+  // Agregar un evento de envío al formulario
+  form.addEventListener("submit", function (event) {
+    console.log("Envío de formulario detectado");
+
+    // Evitar que el formulario se envíe normalmente
+    event.preventDefault();
+
+    // Realizar la petición POST al servidor
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", form.action, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // Preparar los datos a enviar
+    var formData = new FormData(form);
+    console.log("Datos del formulario:", formData);
+
+    // Manejar la respuesta del servidor
+    xhr.onload = function () {
+      console.log("Respuesta del servidor recibida");
+      if (xhr.status === 200) {
+        console.log("Estado de la respuesta: 200 OK");
+
+        // Verificar si el servidor respondió con éxito
+        var response = JSON.parse(xhr.responseText);
+        console.log("Respuesta del servidor:", response);
+
+        if (response.success) {
+          // Mostrar el mensaje de éxito
+          console.log("Envío exitoso. Mostrando mensaje de éxito.");
+          document.querySelector(".success-message").style.display = "block";
+          document.querySelector(".error-message").style.display = "none";
+          form.reset(); // Limpiar el formulario después del envío exitoso
+        } else {
+          // Mostrar el mensaje de error
+          console.log("Error en el envío. Mostrando mensaje de error.");
+          document.querySelector(".success-message").style.display = "none";
+          document.querySelector(".error-message").style.display = "block";
+        }
+      }
+    };
+
+    // Enviar los datos del formulario al servidor
+    console.log("Enviando datos del formulario al servidor...");
+    xhr.send(new URLSearchParams(formData));
+  });
+});
