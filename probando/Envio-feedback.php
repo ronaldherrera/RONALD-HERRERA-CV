3b80a395dc21d5cecf952b1e8b5476543000040b
ronaldherrera <ronaldcalzadilla31@gmail.com>
2024-03-28 19:03:50 +0100

    ccccc

--- probando/Envio-feedback.php
+++ probando/Envio-feedback.php
@@ -1,29 +1,26 @@
 <?php
 // Recoger los datos del formulario
-$mensaje = $_POST['"imput-home"']; // El nombre del textarea del formulario
+$mensaje = $_POST["imput-home"]; // El nombre del textarea del formulario
 
 // Establecer la dirección de correo electrónico de destino
-$destinatario = 'ronaldherrera3d@gmail.com';
+$to = 'ronaldherrera3d@gmail.com';
 
 // Establecer el asunto del correo electrónico
 $asunto = 'Feedback desde el formulario de tu sitio web';
 
 // Establecer el contenido del correo electrónico
-$contenido_correo = '<html><body>';
-$contenido_correo .= '<h2>Feedback recibido desde el formulario de tu sitio web:</h2>';
-$contenido_correo .= '<p>' . $mensaje . '</p>';
-$contenido_correo .= '</body></html>';
+$body = "$mensaje"
 
 // Establecer la dirección de correo electrónico del remitente (debe ser una dirección válida en tu servidor)
-$remitente = 'hola@ronaldherrera.es';
+$remitente = "From: hola@ronaldherrera.es";
 
 // Cabeceras del correo electrónico
-$headers = "From: $remitente\r\n";
-$headers .= "Reply-To: $remitente\r\n";
+$headers = "From: $to\r\n";
+$headers .= "Reply-To: $to\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
 // Enviar el correo electrónico
-if(mail($destinatario, $asunto, $contenido_correo, $headers)) {
+if(mail($to, $asunto, $body, $headers)) {
     // Envío exitoso
     echo json_encode(array('status' => 'success'));
 } else {
