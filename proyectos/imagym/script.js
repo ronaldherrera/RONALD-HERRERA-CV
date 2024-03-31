22f06629348fe663944d675f411b6b3455360e43
ronaldherrera <ronaldcalzadilla31@gmail.com>
2024-03-31 23:48:46 +0200

    NUEVO PROYECTO

--- proyectos/imagym/script.js
+++ proyectos/imagym/script.js
@@ -1,3 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("main section");
+  const links = document.querySelectorAll(".navMain a");
+
+  document.getElementById("estrategia").classList.add("seccion-activa");
+
+  links.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      sections.forEach(function (section) {
+        section.classList.remove("seccion-activa");
+      });
+
+      links.forEach(function (link) {
+        link.classList.remove("activo");
+      });
+
+      const targetId = link.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+      targetSection.classList.add("seccion-activa");
+      link.classList.add("activo");
+
+      // Scroll al inicio de la sección activada
+      targetSection.scrollIntoView({ behavior: "smooth", block: "start" });
+    });
+  });
+});
+
+//////////
+///////////////////////////////////////////////
 //las lineas de footer siguen al raton
 document.addEventListener("mousemove", function (event) {
   var mouseX = event.clientX;
