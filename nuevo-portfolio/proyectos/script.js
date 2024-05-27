3f9919bb44bca6a89bc99ec3141738817942e5c3
ronaldherrera <ronaldcalzadilla31@gmail.com>
2024-05-27 11:39:50 +0200

    .

--- nuevo-portfolio/proyectos/script.js
+++ nuevo-portfolio/proyectos/script.js
@@ -8,3 +8,25 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+/*añadir clase "activo" a los puntos de scroll*/
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll("section");
+  const puntos = document.querySelectorAll(".puntos-scroll div");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const punto = document.querySelector(`#${entry.target.id}P`);
+        if (entry.isIntersecting) {
+          punto.classList.add("activo");
+        } else {
+          punto.classList.remove("activo");
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+  sections.forEach((section) => observer.observe(section));
+});
