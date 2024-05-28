f0facb238e0b6eed1dccc2779c820558c9a9be3a
ronaldherrera <ronaldcalzadilla31@gmail.com>
2024-05-29 01:49:10 +0200

    .

--- nuevo-portfolio/proyectos/script.js
+++ nuevo-portfolio/proyectos/script.js
@@ -31,18 +31,60 @@ document.addEventListener("DOMContentLoaded", () => {
   sections.forEach((section) => observer.observe(section));
 });
 
-/*Abrir/cerrar modal del proyecto*/
-// Selecciona el botón para ver el proyecto y el modal
-const verProyectoBtn = document.getElementById("verProyecto");
-const modalProyecto = document.querySelector(".modal-proyecto");
-const cerrarModalBtn = document.querySelector(".cerrar-modal");
+// Función para abrir un modal
+function abrirModal(triggerId, modalClass) {
+  const triggerElement = document.getElementById(triggerId);
+  const modalElement = document.querySelector(`.modal-proyecto.${modalClass}`);
+  const mainElement = document.querySelector("main"); // Selección del elemento main
 
-// Función para mostrar el modal
-verProyectoBtn.addEventListener("click", () => {
-  modalProyecto.classList.remove("oculto");
-});
+  if (triggerElement && modalElement) {
+    triggerElement.addEventListener("click", () => {
+      modalElement.classList.remove("oculto");
+      mainElement.classList.add("no-scroll"); // Agregar la clase al elemento main
+    });
+  }
+}
 
-// Función para ocultar el modal
-cerrarModalBtn.addEventListener("click", () => {
-  modalProyecto.classList.add("oculto");
+// Función para cerrar un modal
+function cerrarModal(closeClass, modalClass) {
+  const closeElement = document.querySelector(`.cerrar-modal.${closeClass}`);
+  const modalElement = document.querySelector(`.modal-proyecto.${modalClass}`);
+  const mainElement = document.querySelector("main"); // Selección del elemento main
+
+  if (closeElement && modalElement) {
+    closeElement.addEventListener("click", () => {
+      modalElement.classList.add("oculto");
+      mainElement.classList.remove("no-scroll"); // Remover la clase del elemento main
+    });
+  }
+}
+//////////proyectos
+//
+// Abrir/cerrar modal de Subversum
+abrirModal("verSubversum", "subversum");
+cerrarModal("cerrar-subversum", "subversum");
+
+// Abrir/cerrar modal de Imagym
+abrirModal("verImagym", "imagym");
+cerrarModal("cerrar-imagym", "imagym");
+
+// Abrir/cerrar modal de Mahou
+abrirModal("verMahou", "mahou");
+cerrarModal("cerrar-mahou", "mahou");
+
+//
+/////////////fin proyectos
+
+/*limpiar modal y scrol al recagar pagina*/
+document.addEventListener("DOMContentLoaded", function () {
+  const modalElements = document.querySelectorAll(".modal-proyecto");
+  const mainElement = document.querySelector("main");
+
+  // Ocultar todos los modales al cargar la página
+  modalElements.forEach(function (modal) {
+    modal.classList.add("oculto");
+  });
+
+  // Remover la clase no-scroll del main al cargar la página
+  mainElement.classList.remove("no-scroll");
 });
