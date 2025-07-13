9c7c8a91cba54d67a6c18e404528fbabdcf10fcf
ronaldherrera <ronaldcalzadilla31@gmail.com>
2025-07-13 22:16:18 +0200

    .

--- proyectos/aquanote/config.php
+++ proyectos/aquanote/config.php
@@ -2,13 +2,12 @@
 $host = 'localhost';
 $dbname = 'u656964704_aquanote';
 $user = 'u656964704_root';
-$pass = '7HolaRonald*';
+$pass = 'd3&b#$#U$9Hn';
 
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "✅ Conexión exitosa a la base de datos.";
 } catch (PDOException $e) {
-    echo "❌ Error: " . $e->getMessage();
+    die("Error al conectar con la base de datos: " . $e->getMessage());
 }
 ?>
