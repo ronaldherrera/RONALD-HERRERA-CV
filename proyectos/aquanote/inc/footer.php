ea9604420bb7ff74eb03213821ad9fe1dee4a7b8
ronaldherrera <ronaldcalzadilla31@gmail.com>
2025-07-15 00:15:12 +0200

    .

--- proyectos/aquanote/inc/footer.php
+++ proyectos/aquanote/inc/footer.php
@@ -2,7 +2,7 @@
 // inc/footer.php
 ?>
 <footer>
-    <p>&copy; <?= date('Y') ?> Aquanote. Todos los derechos reservados. Diseñado y desarrollado por <a href="https://ronaldherrera.es/">Ronald Herrera</a></p>
+    <p>Diseñado y desarrollado por <a href="https://ronaldherrera.es/">Ronald Herrera</a> <br>&copy; <?= date('Y') ?> Aquanote. Todos los derechos reservados.</p>
 </footer>
 </body>
 </html>
