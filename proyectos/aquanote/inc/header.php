c6812185cbbec0505710baeb397fa428e0f49c91
ronaldherrera <ronaldcalzadilla31@gmail.com>
2025-07-14 13:38:53 +0200

    .

--- proyectos/aquanote/inc/header.php
+++ proyectos/aquanote/inc/header.php
@@ -6,7 +6,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aquanote</title>
-    <link rel="stylesheet" href="../">
+    <link rel="stylesheet" href="/proyectos/aquanote/css/style.css">
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            const toggle = document.getElementById("menu-toggle");
+            const container = document.getElementById("menu-contenedor");
+            if (toggle && container) {
+                toggle.addEventListener("click", () => {
+                    container.classList.toggle("open");
+                });
+            }
+        });
+    </script>
 </head>
 <body>
 <header>
@@ -14,5 +25,13 @@
     <div class="logo">
         <img src="/proyectos/aquanote/img/logo-horizontal.svg" alt="Aquanote" height="40">
     </div>
+    <div class="menu-contenedor" id="menu-contenedor">
+        <button id="menu-toggle" class="menu-toggle">☰</button>
+        <div class="menu-dropdown">
+            <a href="../index.php">Inicio</a>
+            <a href="../cuenta.php">Mi cuenta</a>
+            <a href="../mi-acuario.php">Mi acuario</a>
+            <a href="../logout.php">Cerrar sesión</a>
+        </div>
     </div>
 </header>
