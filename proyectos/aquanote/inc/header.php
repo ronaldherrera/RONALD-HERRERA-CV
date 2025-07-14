37b82a97be6d2a1ef028c4858dbc2d08ee973053
ronaldherrera <ronaldcalzadilla31@gmail.com>
2025-07-14 11:39:11 +0200

    .

--- proyectos/aquanote/inc/header.php
+++ proyectos/aquanote/inc/header.php
@@ -6,12 +6,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aquanote</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 <header>
     <div class="logo">
-        <img src="/img/logo.svg" alt="Aquanote" height="40">
+        <img src="../img/logo-horizontal" alt="Aquanote" height="40">
         <span>Aquanote</span>
     </div>
 </header>
